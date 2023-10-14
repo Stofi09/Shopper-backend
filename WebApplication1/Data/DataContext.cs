21c1b0677adc687e5f241c104df15a80a5f3200e
Stofi09 <kristof.solak@gmail.com>
2023-10-14 19:10:36 +0200

    User table added

--- WebApplication1/Data/DataContext.cs
+++ WebApplication1/Data/DataContext.cs
@@ -9,6 +9,7 @@ namespace WebApplication1.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ShopUser> ShopUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
