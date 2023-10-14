3f3c6946d70ca68c669f13f0c78c85bf5dfb02d0
Stofi09 <kristof.solak@gmail.com>
2023-10-14 19:02:09 +0200

    Token generator completed

--- WebApplication1/Utilities/TokenGenerator.cs
+++ WebApplication1/Utilities/TokenGenerator.cs
@@ -1,6 +1,32 @@
-﻿namespace WebApplication1.Utilities
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebApplication1.Utilities
 {
-    public class TokenGenerator
+    public class TokenGenerator : ITokenGenerator
     {
+        public string GenerateUserToken(string secretKey, int expiryMinutes, string id)
+        {
+            var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+            var securityKey = new SymmetricSecurityKey(keyBytes);
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, id)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "KingdomConquerorsMakers",
+                audience: "Players",
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(expiryMinutes),
+                signingCredentials: credentials);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
