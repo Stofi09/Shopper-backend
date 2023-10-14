eb5087acb6257bfd219d3d4f93b82ce772badd8e
Stofi09 <kristof.solak@gmail.com>
2023-10-14 19:05:29 +0200

    User created

---
+++ WebApplication1/Models/ShopUser.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class ShopUser
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        private string _password;
+        [Required]
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = BCrypt.Net.BCrypt.HashPassword(value);
+            }
+        }
+    }
+}
