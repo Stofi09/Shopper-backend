914f404938ce681fab012ff375f596744c47cd9a
Stofi09 <kristof.solak@gmail.com>
2023-10-14 19:09:45 +0200

    User service created

---
+++ WebApplication1/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public interface IUserService
+    {
+        public void SetUserPassword(ShopUser user, string password);
+    }
+}
---
+++ WebApplication1/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class UserService : IUserService
+    {
+        public void SetUserPassword(ShopUser user, string password)
+        {
+            user.Password = BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+    }
+}
