5176f1b40a5ddc99f1f207b459bed0729aee9db8
Stofi09 <kristof.solak@gmail.com>
2023-10-14 19:10:12 +0200

    User fields updated

--- WebApplication1/Models/ShopUser.cs
+++ WebApplication1/Models/ShopUser.cs
@@ -5,21 +5,14 @@ namespace WebApplication1.Models
     public class ShopUser
     {
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
+        [Required, EmailAddress]
         public string Email { get; set; }
-        private string _password;
+
         [Required]
-        public string Password
-        {
-            get
-            {
-                return _password;
-            }
-            set
-            {
-                _password = BCrypt.Net.BCrypt.HashPassword(value);
-            }
-        }
+        public string Password { get; set; }
     }
 }
