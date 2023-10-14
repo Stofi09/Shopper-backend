bf59f8b0ccd1be5903565a8ff26d7e8357e3752d
Stofi09 <kristof.solak@gmail.com>
2023-10-14 18:41:12 +0200

    First init

---
+++ WebApplication1/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
